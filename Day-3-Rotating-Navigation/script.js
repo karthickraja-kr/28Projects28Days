2c8169ed2ceafb3270fa1275f5c4b29651fa0ef8
karthickraja-kr <karthickrajasty@gmail.com>
2021-02-06 20:25:19 +0530

    Day-3 Finshed

---
+++ Day-3-Rotating-Navigation/script.js
@@ -0,0 +1,8 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+
+const container = document.querySelector(".container");
+
+open.addEventListener("click", () => container.classList.add("active"));
+
+close.addEventListener("click", () => container.classList.remove("active"));
